9b44a3113492d2d459827895b6547f5811ff902d
HananeDUP <hanane.moualil@gmail.com>
2018-11-22 21:04:01 +0100

    insert in db done, create table of all navire data in progress

--- app/routes/ship_postgres.js
+++ app/routes/ship_postgres.js
@@ -9,71 +9,99 @@ router.post('/ship/register', (req, res, next) => {
     const destinataire = req.body.destinataire;
     const produit = req.body.produit;
     const arrivee = req.body.arrivee;
+    const arriveestr = '\''.concat(arrivee, "'")
     const depart = req.body.depart;
+    const departstr = '\''.concat(depart, "'")
     const silo = req.body.silo;
     const portchrgmt = req.body.portchrgmt;
     const portdechrgmt = req.body.portdechrgmt;
     const quantite = req.body.quantite;
+    const myquery = { navire_id: 'navire.id', importateur_id: 'importateur.id', destinataire_id: 'destinataire.id', produit_id: 'produit.id', silo_id: 'silo.id', port_chargement_id: 'port_chargement.id', port_dechargement_id: 'port_dechargement.id' }
+    console.log('navire est', navire)
+    console.log('importateur est', importateur)
     // var myquery=format(`SELECT * from db where client=${client}`)
     //where(`client=${client}`)
-    let myarrayres= db.select().from("ship").where('name',destinataire).then(data=>res.send(data))
-    console.log(myarrayres)
-//     knex('table').insert({a: 'b'}).returning('*').toString();
-//     db('navire_data').insert()
-//     rows = [{...}, {...}];
-// "
-//     INSERT INTO navire_data (date_arrivee, date_depart, navire_id, importateur_id, destinataire_id, produit_id, silo_id, port_chargement_id, port_dechargement_id, quantite_bl, quantite_restante) 
-// SELECT '10-11-2018','En cours',navire.id, importateur.id, destinataire.id,produit.id,silo.id
-// ,port_chargement.id,port_dechargement.id,10000,10000
-// FROM navire CROSS JOIN importateur CROSS JOIN destinataire CROSS JOIN produit CROSS JOIN silo CROSS JOIN port_chargement CROSS JOIN port_dechargement
-//     WHERE navire.nom IN ('NEUTRINO') 
-// 	and importateur.nom IN ( 'Alimaroc') 
-// 	and destinataire.nom IN ('Agro Atlas')
-// 	and produit.nom IN('Mais')
-// 	and silo.nom in ('SOSIPO') 
-// 	and port_chargement.ville in ('Itaqui')
-//     and port_dechargement.ville in ('Casablanca')
-//     "
+    // db.select('*').from('navire').where({
+    db('navire').crossJoin('importateur').crossJoin('destinataire').crossJoin('produit').crossJoin('silo')
+        .crossJoin('port_chargement').crossJoin('port_dechargement')
+        .where({
+            'navire.nom': navire,
+            'importateur.nom': importateur,
+            'destinataire.nom': destinataire,
+            'produit.nom': produit,
+            'silo.nom': silo,
+            'port_chargement.ville': portchrgmt,
+            'port_dechargement.ville': portdechrgmt
+        }).select(myquery)
+        .then(data => {
+            console.log('dataaa', data)
+            var newdict = data[0]
+            newdict.date_arrivee = arrivee
+            newdict.date_depart = depart
+            newdict.quantite_bl = parseInt(quantite)
+            newdict.quantite_restante = parseInt(quantite)
+            // newdict.push({key:'quantite_bl',value:quantite})
+            console.log('newdict=', newdict)
+            db('navire_data').select().where(newdict)
+                .then(dataset => {
+                    console.log('dat',dataset)
+                    console.log('dat',typeof(dataset))
+                    if (dataset.length == 0) {
+                        console.log('newdict',newdict)
+                        db('navire_data').insert([newdict])
+                        .then(()=> {
+                            console.log('okokok')
+                            res.json({status:'ok'})
+                        })
+                        .catch(err=>{
+                            console.log('bouhh', err)
+                            res.json({status:'error'})
+                        })
+                    }
+                    else {
+                        res.json({status:'error'})
+                        console.log("erroorrr",'dataselect=', dataset)
+                    }
+                })
+                .catch(err=>{
+                    console.log("first errooooooor", err)
+                    res.json({status:'error'})
+                })
 
+            // return db('navire_data').insert([newdict])
+            // return new Promise((resolve, reject) => {
+            //     insert(data, () => resolve())
+            // })
 
+            // var valuesdict = Object.keys(data[0]).map(function(key){
+            //     return data[0][key];
+            // }); 
+            // valuesdict.unshift(arrivee,depart)
+            // valuesdict.push(parseInt(quantite),parseInt(quantite))
 
+        })
+        .catch(err=>{
+                    res.json({status:'error'})
+                    console.log("what's the errrrrrrrrrrrroooooorrrrr", err)
+        })
+})
 
-// "insert into "table" ("a") values ('b')"
-    // console.log(db('ship').select())
-    // Ship.findOne({ client,ship,portload,portunload,arrival,product,quantity, receiver}, (err, foundShip) => {
-    //     if (err) {
-    //         res.status(500).json({ message: "Ship check went bad." });
-    //         return;
-    //     }
-
-    //     if (foundShip) {
-    //         res.status(400).json({ message: 'Ship already enregistred'});
-    //         return;
-    //     }
-
-    //     const newShip = new Ship({
-    //         client : client,
-    //         ship : ship,
-    //         portload : portload,
-    //         portunload : portunload,
-    //         arrival : arrival,
-    //         departure : departure,
-    //         silo : silo,
-    //         product : product,
-    //         quantity : quantity,
-    //         receiver : receiver,
-    //         transporter : transporter,
-    //     });
-
-    //     newShip.save((err) => {
-    //         if (err) {
-    //             res.status(400).json({ message: 'Saving ship to database went wrong.' });
-    //             return;
-    //         }
-    //         res.status(200).json({newShip, message:"Ship well registred"});
-    //     })
-
-    // })
+router.post('/ship/details', (req, res, next) => {
+    const dateFrom = req.body.dateFrom;
+    const dateTo = req.body.dateTo;
+    const myquery = {date:'date_arrivee',navire_nom:'navire.nom',importateur_nom:'importateur.nom',destinataire_nom:'destinataire.nom',produit_nom:'produit.nom',silo_nom:'silo.nom',quantite_BL:'quantite_bl',quantite_restante:'quantite_restante'}
+    // db('navire_data').select().where('date_arrivee','>=',dateFrom).andWhere('date_arrivee','<=',dateTo)
+    // .then(data=>res.json(data))
+    // .catch(err=>res.json({status:'error'}))
+    db('navire_data').innerJoin('navire', 'navire_data.navire_id', '=', 'navire.id')
+    .innerJoin('importateur', 'navire_data.importateur_id', '=', 'importateur.id')
+    .innerJoin('destinataire', 'navire_data.destinataire_id', '=', 'destinataire.id')
+    .innerJoin('silo', 'navire_data.silo_id', '=', 'silo.id')
+    .innerJoin('produit', 'navire_data.produit_id', '=', 'produit.id')
+    .select(myquery)
+    .where('date_arrivee','>=',dateFrom).andWhere('date_arrivee','<=',dateTo)
+    .then(data=>res.json(data))
+    .catch(err=>res.json({status:'error'}))
 })
 
 module.exports = router;
